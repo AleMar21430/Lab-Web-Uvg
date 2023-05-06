8716a2635464fa3703bd411709fa9b904a42a9ef
God Chama <99276653+AleMar21430@users.noreply.github.com>
2023-05-06 13:04:00 -0600

    Commit

--- Proyecto 1/index.js
+++ Proyecto 1/index.js
@@ -3,9 +3,6 @@ window.onload = () => {
 }
 
 function createElements() {
-	let test = document.createElement("div")
-	test.innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-	document.body.appendChild(test)
 	let Navbar = document.getElementById("id-top-navbar")
 	let li = document.createElement("li")
 	let a = document.createElement("a")
@@ -17,4 +14,15 @@ function createElements() {
 
 	li.appendChild(a)
 	Navbar.appendChild(li)
+}
+
+function slide() {
+	let value = document.getElementById("Video_Slider").value
+	document.getElementById("Video").style.clipPath = "polygon(0 0, " + value + "% 0, " + value + "% 100%, 0 100%)"
+	if (value > 78) {
+		document.getElementById("Label_Left").style.clipPath = "polygon(100% 0, " + (value-77)*5 + "% 0, " + (value-77)*5 + "% 100%, 100% 100%)"
+	} else {
+		document.getElementById("Label_Left").style.clipPath = "polygon(100% 0, 0 0, 0 100%, 100% 100%)"
+	}
+	
 }
