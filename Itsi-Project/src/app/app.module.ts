d26eec91876f066ea57f968095737c53539aa644
God Chama <123910828+Raylight-Developer@users.noreply.github.com>
2023-05-18 19:14:37 -0600

    Commit

---
+++ Itsi-Project/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireAuthModule,
+		AngularFirestoreModule,
+		AngularFireStorageModule,
+		AngularFireDatabaseModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
