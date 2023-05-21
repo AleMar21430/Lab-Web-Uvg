aca76a9daaf260119399f0371f28984273084107
God Chama <123910828+Raylight-Developer@users.noreply.github.com>
2023-05-21 13:23:02 -0600

    Working

--- Itsi-Project/src/environments/environment.prod.ts
+++ Itsi-Project/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyCxVXfIhl396EcSxGdaKbyFqFyuVjzb_Sc",
+		authDomain: "itsi-project.firebaseapp.com",
+		databaseURL: "https://itsi-project-default-rtdb.firebaseio.com",
+		projectId: "itsi-project",
+		storageBucket: "itsi-project.appspot.com",
+		messagingSenderId: "56782634172",
+		appId: "1:56782634172:web:250a436af65bcbe20e64b9",
+		measurementId: "G-SVFNFZ11WL"
+	}
 };
