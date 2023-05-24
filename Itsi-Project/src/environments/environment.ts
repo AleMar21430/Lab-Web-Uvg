a02878957703bc61297443c297a198988f0e0713
Peko-Chama <99276653+AleMar21430@users.noreply.github.com>
2023-05-24 15:05:48 -0600

    Update environment.ts

--- Itsi-Project/src/environments/environment.ts
+++ Itsi-Project/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: "AIzaSyCxVXfIhl396EcSxGdaKbyFqFyuVjzb_Sc",
-		authDomain: "itsi-project.firebaseapp.com",
-		databaseURL: "https://itsi-project-default-rtdb.firebaseio.com",
-		projectId: "itsi-project",
-		storageBucket: "itsi-project.appspot.com",
-		messagingSenderId: "56782634172",
-		appId: "1:56782634172:web:250a436af65bcbe20e64b9",
-		measurementId: "G-SVFNFZ11WL"
+		apiKey: "",
+		authDomain: "",
+		databaseURL: "",
+		projectId: "",
+		storageBucket: "",
+		messagingSenderId: "",
+		appId: "",
+		measurementId: ""
 	}
 };
