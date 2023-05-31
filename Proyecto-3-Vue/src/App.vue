5bdb4e6671d18b6f83551b5127639c54c652172b
God Chama <99276653+AleMar21430@users.noreply.github.com>
2023-05-31 10:48:28 -0600

    Commit

---
+++ Proyecto-3-Vue/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+	import BG from './assets/Bg.png'
+</script>
+
+<template>
+	<main>
+		<div class="Block">
+			<div class="Background Parallax" pr-val="10" :style="{backgroundImage: `url(${BG})` }">
+				<h1 class="Parallax" pr-val="40">Alejandro Martínez</h1>
+				<h2 class="Parallax" pr-val="25">21430</h2>
+			</div>
+			<div>
+				<h1 class="Parallax" pr-val="5">Alejandro Martínez</h1>
+				<h2 class="Parallax" pr-val="2.5">21430</h2>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script lang="ts">
+	(function() {
+		document.addEventListener("mousemove", parallax);
+		const elem = document.querySelector("#parallax");
+
+		function parallax(event: MouseEvent) {
+			const parallaxSpans = document.querySelectorAll('.Parallax');
+			parallaxSpans.forEach((shift: any) => {
+				const position = shift.getAttribute('pr-val');
+				const x = ((event.pageX - window.innerWidth/2) * Number(position)) / 500 ;
+				const y = ((event.pageY - window.innerHeight/2) * Number(position)) / 500;
+				shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+			});
+		}
+	})();
+</script>
