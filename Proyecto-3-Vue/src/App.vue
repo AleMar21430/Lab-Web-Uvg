31945430938e36227c18101f881c189491eabaec
God Chama <99276653+AleMar21430@users.noreply.github.com>
2023-06-01 09:28:29 -0600

    Commit

--- Proyecto-3-Vue/src/App.vue
+++ Proyecto-3-Vue/src/App.vue
@@ -35,46 +35,56 @@
 		Container.forEach((Item: any) => {
 			const multiplier = Item.getAttribute('scroll-mult') || 1;
 			if (window.innerWidth > 600) {
-				const x = (window.scrollY * - Number(multiplier));
-				Item.style.transform = `translateX(${x}px)`;
+				const y = (window.scrollY * Number(multiplier));
+				Item.style.transform = `translateY(${y}px)`;
 			} else {
-				const x = (window.scrollY * - Number(multiplier)*1.5);
-				Item.style.transform = `translateX(${x}px)`;
+				const y = (window.scrollY * Number(multiplier)*1.5);
+				Item.style.transform = `translateY(${y}px)`;
 			}
 		});
 
+		const Abs = document.getElementById("Abs")
+		if (Abs) {
+			Abs.style.transform = `translateY(${window.scrollY}px)`
+		}
+
 		const Header = document.getElementById("Scroll_Area")
-		if (Header)
-		if (window.innerWidth > 600) {
-			if (window.scrollY > 5500) {
-				Header.style.transform = `translateY(${(window.scrollY - 5500) * -0.1}px)`
+		if (Header) {
+			if (window.innerWidth > 600) {
+				if (window.scrollY > 4500) {
+					Header.style.transform = `translateX(${(window.scrollY - 4500) * -0.1}px)`
+				} else {
+					Header.style.transform = `translateX(0px)`
+				}
 			} else {
-				Header.style.transform = `translateY(0px)`
-			}
-		} else {
-			if (window.scrollY > 1200) {
-				Header.style.transform = `translateY(${(window.scrollY - 1200) * -0.15}px)`
-			} else {
-				Header.style.transform = `translateY(0px)`
+				if (window.scrollY > 1200) {
+					Header.style.transform = `translateX(${(window.scrollY - 1200) * -0.15}px)`
+				} else {
+					Header.style.transform = `translateX(0px)`
+				}
 			}
 		}
-		
+
 		const LeftSlide = document.querySelectorAll('.Slide_Left');
 		LeftSlide.forEach((Item: any) => {
 			const multiplier = Item.getAttribute('scroll-mult') || 0;
+			const alpha = Item.getAttribute('scroll-alpha') || 1;
 
 			if (window.innerWidth > 600) {
 				const x = - window.scrollY + Number(multiplier);
+				const a = 0 + (window.scrollY - Number(alpha)) * 0.002;
 
-				if (x < -window.innerWidth) {
+				if (x < - window.innerWidth) {
 					Item.style.transform = 'translateX(-100vw)';
+					Item.style.opacity = 1
 				} else {
 					Item.style.transform = `translateX(${x}px)`;
+					Item.style.opacity = a
 				}
 			} else {
 				const x = - window.scrollY + Number(multiplier) * 1.5;
 
-				if (x < -window.innerWidth) {
+				if (x < - window.innerWidth) {
 					Item.style.transform = 'translateX(-100vw)';
 				} else {
 					Item.style.transform = `translateX(${x}px)`;
@@ -84,14 +94,18 @@
 		const RightSlide = document.querySelectorAll('.Slide_Right');
 		RightSlide.forEach((Item: any) => {
 			const multiplier = Item.getAttribute('scroll-mult') || 0;
+			const alpha = Item.getAttribute('scroll-alpha') || 1;
 
 			if (window.innerWidth > 600) {
 				const x = window.scrollY - Number(multiplier);
+				const a = 0 + (window.scrollY - Number(alpha)) * 0.002;
 
 				if (x > window.innerWidth) {
 					Item.style.transform = 'translateX(100vw)';
+					Item.style.opacity = 1
 				} else {
 					Item.style.transform = `translateX(${x}px)`;
+					Item.style.opacity = a
 				}
 			} else {
 				const x = window.scrollY - Number(multiplier) * 1.5;
@@ -135,6 +149,46 @@
 <template>
 	<main>
 		<div class="Column">
+			<div class="Abs" id="Abs">
+				<div class="Scroll_Area" id="Scroll_Area">
+					<div class="Scroll_Slide" scroll-mult="0.6">
+						<img class="Technology S" src="https://skillicons.dev/icons?i=html"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=css"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=scss"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=blender"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=unreal"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=figma"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=ps"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=pr"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=ae"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=qt"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=js"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=ts"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=cpp"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=rust"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=py"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=vscode"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=visualstudio"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=sqlite"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=postgres"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=java"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=nodejs"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=nextjs"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=angular"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=react"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=flutter"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=vite"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=vue"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=webpack"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=babel"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=github"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=git"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=aws"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=nginx"/>
+						<img class="Technology S" src="https://skillicons.dev/icons?i=firebase"/>
+					</div>
+				</div>
+			</div>
 
 			<div class="Card">
 				<div
@@ -149,52 +203,20 @@
 				</div>
 			</div>
 
-			<div class="Scroll_Area" id="Scroll_Area">
-				<div class="Scroll_Slide" scroll-mult="0.5">
-					<img class="Technology S" src="https://skillicons.dev/icons?i=html"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=css"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=scss"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=blender"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=unreal"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=figma"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=ps"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=pr"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=ae"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=qt"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=js"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=ts"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=cpp"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=rust"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=py"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=vscode"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=visualstudio"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=sqlite"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=postgres"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=java"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=nodejs"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=nextjs"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=angular"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=react"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=flutter"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=vite"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=vue"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=webpack"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=babel"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=github"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=git"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=aws"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=nginx"/>
-					<img class="Technology S" src="https://skillicons.dev/icons?i=firebase"/>
-				</div>
-			</div>
-			
-			<iframe class="Block Slide_Left" scroll-mult="-1000" src="https://itsi-project.web.app/hogar"></iframe>
-			<iframe class="Block Slide_Right" scroll-mult="-250" src="https://itsi-database.web.app/hogar"></iframe>
-			<iframe class="Block Slide_Left" scroll-mult="500" src="https://blender-clone-ssr.web.app/"></iframe>
-			<iframe class="Block Slide_Right" scroll-mult="1250" src="https://old-labs.web.app/Lab%207_2/index.html"></iframe>
-			<iframe class="Block Slide_Left" scroll-mult="2000" src="https://old-labs.web.app/Lab%205_2/Lab6.html"></iframe>
-			<iframe class="Block Slide_Right" scroll-mult="2750" src="https://old-labs.web.app/Lab%204/Lab4.html"></iframe>
-			<iframe class="Block Slide_Left" scroll-mult="3500" src="https://old-labs.web.app/Lab%203/Lab3_Home.html"></iframe>
+			<h4 class="Slide_Left" scroll-mult="-1150" scroll-alpha="50">Proyecto 2</h4>
+			<iframe class="Block Slide_Right" scroll-mult="-1000" scroll-alpha="250" src="https://itsi-project.web.app/hogar"></iframe>
+			<h4 class="Slide_Left" scroll-mult="-400" scroll-alpha="900">Proyecto 2</h4>
+			<iframe class="Block Slide_Right" scroll-mult="-150" scroll-alpha="1100" src="https://itsi-database.web.app/hogar"></iframe>
+			<h4 class="Slide_Left" scroll-mult="450" scroll-alpha="1750">Proyecto 1</h4>
+			<iframe class="Block Slide_Right" scroll-mult="700" scroll-alpha="1950" src="https://blender-clone-ssr.web.app/"></iframe>
+			<h4 class="Slide_Left" scroll-mult="1300" scroll-alpha="2600">Laboratorio 7</h4>
+			<iframe class="Block Slide_Right" scroll-mult="1550" scroll-alpha="2800" src="https://old-labs.web.app/Lab%207_2/index.html"></iframe>
+			<h4 class="Slide_Left" scroll-mult="2150" scroll-alpha="3450">Laboratorio 6</h4>
+			<iframe class="Block Slide_Right" scroll-mult="2400" scroll-alpha="3650" src="https://old-labs.web.app/Lab%205_2/Lab6.html"></iframe>
+			<h4 class="Slide_Left" scroll-mult="3000" scroll-alpha="4300">Laboratorio 4</h4>
+			<iframe class="Block Slide_Right" scroll-mult="3250" scroll-alpha="4500" src="https://old-labs.web.app/Lab%204/Lab4.html"></iframe>
+			<h4 class="Slide_Left" scroll-mult="3850" scroll-alpha="5150">Laboratorio 3</h4>
+			<iframe class="Block Slide_Right" scroll-mult="4100" scroll-alpha="5350" src="https://old-labs.web.app/Lab%203/Lab3_Home.html"></iframe>
 
 			<div class="Row Row_Start End_Card Block_Container">
 				<div class="Column">
